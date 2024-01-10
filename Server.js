18a672d718015f78a427878912c475d9df8c87d3
softrdinesh <softrdinesh@gmail.com>
2024-01-10 23:08:13 +0800

    Update Server.js

--- Server.js
+++ Server.js
@@ -40,6 +40,6 @@ io.on("connection", (socket) => {
   });
 });
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, "example-socket-app.onrender.com", () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
